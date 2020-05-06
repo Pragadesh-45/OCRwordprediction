9c90ae4b10ac51d396608096ed63c2882f729956
Pragadesh-45 <54320162+Pragadesh-45@users.noreply.github.com>
2020-05-06 16:36:54 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,9 @@
-
+#include <stdio.h>
+int main(){
+  FILE *fp=fopen("mytext.txt","r");
+  int count=0;
+  char c;
+  while((c=fgetc(fp) && c!=EOF){
+    ++count;}
+  printf("The number of characters is : %d",count);
+  return 0;}
